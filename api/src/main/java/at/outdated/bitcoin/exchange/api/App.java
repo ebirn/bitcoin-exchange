09ce80838ce642645262a2698f65514a7d48b9bd
Erich Birngruber <ebirn@outdated.at>
2013-05-23 12:23:01 +0200

    add base api for all exchange clients

---
+++ api/src/main/java/at/outdated/bitcoin/exchange/api/App.java
@@ -0,0 +1,13 @@
+package at.outdated.bitcoin.exchange.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
