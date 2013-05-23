5485343a4bf78840f894a03fb2e466b05c877ece
Erich Birngruber <ebirn@outdated.at>
2013-05-24 01:38:29 +0200

    fixed maven structure, added one dummy class

---
+++ mtgox/src/main/java/at/outdated/bitcoin/exchange/mtgox/ExchangeMtgox.java
@@ -0,0 +1,11 @@
+package at.outdated.bitcoin.exchange.mtgox;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ebirn
+ * Date: 24.05.13
+ * Time: 01:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExchangeMtgox {
+}
