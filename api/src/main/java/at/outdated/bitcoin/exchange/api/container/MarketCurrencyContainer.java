e55d6d7459f03ea04f80e3631e19ea35f0cb01f0
Erich Birngruber <erich.birngruber@meduniwien.ac.at>
2013-05-27 12:53:27 +0200

    fix notification handling for price changes

---
+++ api/src/main/java/at/outdated/bitcoin/exchange/api/container/MarketCurrencyContainer.java
@@ -0,0 +1,17 @@
+package at.outdated.bitcoin.exchange.api.container;
+
+import at.outdated.bitcoin.exchange.api.Market;
+import at.outdated.bitcoin.exchange.api.currency.Currency;
+import at.outdated.bitcoin.exchange.api.market.MarketContainer;
+import at.outdated.bitcoin.exchange.api.market.Markets;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ebirn
+ * Date: 27.05.13
+ * Time: 12:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class MarketCurrencyContainer<T> extends MarketContainer<CurrencyContainer<T>> {
+
+}
