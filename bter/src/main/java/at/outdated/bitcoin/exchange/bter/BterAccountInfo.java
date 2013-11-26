2ca8017097b49a97db811e2140952671932664e8
Erich Birngruber <ebirn@outdated.at>
2013-11-27 00:23:20 +0100

    add basic fee info

--- bter/src/main/java/at/outdated/bitcoin/exchange/bter/BterAccountInfo.java
+++ bter/src/main/java/at/outdated/bitcoin/exchange/bter/BterAccountInfo.java
@@ -3,6 +3,7 @@ package at.outdated.bitcoin.exchange.bter;
 import at.outdated.bitcoin.exchange.api.account.AccountInfo;
 import at.outdated.bitcoin.exchange.api.market.TradeDecision;
 import at.outdated.bitcoin.exchange.api.market.fee.Fee;
+import at.outdated.bitcoin.exchange.api.market.fee.SimplePercentageFee;
 import at.outdated.bitcoin.exchange.api.market.fee.ZeroFee;
 
 /**
@@ -10,7 +11,7 @@ import at.outdated.bitcoin.exchange.api.market.fee.ZeroFee;
  */
 public class BterAccountInfo extends AccountInfo {
 
-    Fee tradingFee = new ZeroFee();
+    Fee tradingFee = new SimplePercentageFee(0.002);
 
     @Override
     public Fee getTradeFee(TradeDecision trade) {
