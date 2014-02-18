244b47521b709ca42ae7df1d1401404115b6f3b5
Erich Birngruber <erich.birngruber@meduniwien.ac.at>
2014-02-18 16:36:00 +0100

    add summary methods

--- api/src/main/java/at/outdated/bitcoin/exchange/api/account/Balance.java
+++ api/src/main/java/at/outdated/bitcoin/exchange/api/account/Balance.java
@@ -4,8 +4,7 @@ import at.outdated.bitcoin.exchange.api.currency.Currency;
 import at.outdated.bitcoin.exchange.api.currency.CurrencyValue;
 import at.outdated.bitcoin.exchange.api.market.Market;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by ebirn on 25.01.14.
@@ -16,17 +15,22 @@ public class Balance {
     Map<Currency,CurrencyValue> open = new HashMap<>();
 
     public Balance() {
+        initCurrencies(Arrays.asList(Currency.values()));
     }
 
     public Balance(Market market) {
-        for(Currency c : market.getCurrencies()) {
-            available.put(c, new CurrencyValue(c));
-            open.put(c, new CurrencyValue(c));
+        initCurrencies(market.getCurrencies());
+    }
+
+    private void initCurrencies(Collection<Currency> currencies) {
+        for(Currency c : currencies) {
+            this.available.put(c, new CurrencyValue(c));
+            this.open.put(c, new CurrencyValue(c));
         }
     }
 
     public CurrencyValue getOpen(Currency c) {
-        return open.get(c);
+        return this.open.get(c);
     }
 
     public CurrencyValue getAvailable(Currency c) {
@@ -34,21 +38,21 @@ public class Balance {
     }
 
     public void setOpen(CurrencyValue cv) {
-        open.put(cv.getCurrency(), cv);
+        this.open.put(cv.getCurrency(), cv);
     }
 
     public void setAvailable(CurrencyValue cv) {
-        available.put(cv.getCurrency(), cv);
+        this.available.put(cv.getCurrency(), cv);
     }
 
     public boolean isAvailable(Currency c) {
-        return (available.containsKey(c) && available.get(c).isPositive());
+        return (this.available.containsKey(c) && this.available.get(c).isPositive());
     }
 
     public CurrencyValue getTotal(Currency c) {
 
-        CurrencyValue openValue = open.get(c);
-        CurrencyValue availValue = available.get(c);
+        CurrencyValue openValue = this.open.get(c);
+        CurrencyValue availValue = this.available.get(c);
 
 
         CurrencyValue total = new CurrencyValue(c);
@@ -62,17 +66,41 @@ public class Balance {
     public String longString() {
         StringBuilder builder = new StringBuilder();
 
-        for(Currency c : Currency.values()) {
+        boolean hasFunds = false;
+        for(Currency c : this.available.keySet()) {
             CurrencyValue currencyBalance = getTotal(c);
             if(currencyBalance != null && currencyBalance.isPositive()) {
                 builder.append(c.name());
                 builder.append("=");
                 builder.append(currencyBalance.getValue().toString());
                 builder.append(" ");
+                hasFunds = true;
             }
         }
 
+        if(!hasFunds) {
+            return "none.";
+        }
+
         return builder.toString();
+    }
+
+    public boolean isZero() {
+
+        for(Currency c : this.available.keySet()) {
+            if(this.available.get(c).isPositive()) return false;
+            if(this.open.get(c).isPositive()) return false;
+        }
+
+        return true;
+    }
+
+    public boolean hasFunds() {
+        return !isZero();
+    }
+
+    public Set<Currency> currencies() {
+        return new HashSet<>(this.available.keySet());
     }
 
     @Override
